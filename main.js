6299359a38d8fc2000b05ba842853a508580073a
Danihobs <daniel.hobold.santos@escola.pr.gov.br>
2024-06-20 07:53:33 -0300

    Add files via upload
    20/06

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const botoes  = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo")
-for (let i = 0; i < botoes.length ; i++){
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent = "Bibs"
+const tempoObjetiv1 = new Date(2024,11,31,59,59);
+const tempoObjetivo2 = new Date(2024,11,31,59,59);
+const tempoObjetivo3 = new Date(2024,11,31,59,59);
+const tempoObjetivo4 = new Date(2024,11,31,59,59);
+const agora = new Date();
+let segundos;
+let minutos;
+let horas;
+let dias;
+segundos = (tempoObjetiv1-agora)/1000
+minutos = segundos/60
+horas = minutos/60
+dias = horas/24
+tempo[1].textContent = agora
+tempo[0].textContent = tempoObjetiv1-agora
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
-    for (j=0;j<botoes.length;j++){
-        botoes[j].classList.remove("ativo");
-        conteudo[j].classList.remove("ativo")
+        for (let j = 0 ; j < botoes.length ; j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
-    botoes[i].classList.add("ativo")
-    conteudo[i].classList.add("ativo")
-  }
 }
